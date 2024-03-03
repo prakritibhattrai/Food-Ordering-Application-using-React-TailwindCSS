2bb00954097b0bbcb8dfa71a91d42f9207535ce7
prakriti.bhattarai5 <prakriti.bhattarai5@gmail.com>
2024-03-03 01:38:55 -0600

    Initialized project with Parcel bundler for easy development and deployment.

---
+++ App.js
@@ -0,0 +1,610 @@
+import React from 'react'
+import ReactDOM from "react-dom/client"
+
+
+const restaurantList = [
+    {
+        "cuisines": "Italian, Mediterranean, Veal, Pasta, Frozen Food, Lasagna, Panini, Catering",
+        "dietary_preferences": [],
+        "featuredImage": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/514/835/medium/brettone.jpg"
+        },
+        "hoursOfOperation": [
+            [
+                [
+                    540,
+                    1320
+                ]
+            ],
+            [
+                [
+                    540,
+                    1320
+                ]
+            ],
+            [
+                [
+                    540,
+                    1320
+                ]
+            ],
+            [
+                [
+                    540,
+                    1320
+                ]
+            ],
+            [
+                [
+                    540,
+                    1320
+                ]
+            ],
+            [
+                [
+                    540,
+                    1320
+                ]
+            ],
+            [
+                [
+                    540,
+                    1320
+                ]
+            ]
+        ],
+        "id": "14451",
+        "integrations": [
+            {
+                "id": "83286",
+                "location": null,
+                "remote_id": "brettonecatering"
+            }
+        ],
+        "locations": [
+            {
+                "address": {
+                    "geo": {
+                        "lat": 43.779777,
+                        "lng": -79.570968
+                    },
+                    "nickname": "830 Rowntree Dairy Rd, Unit 5, Woodbridge, ON L4L 5V2, Canada"
+                },
+                "id": "15783"
+            }
+        ],
+        "logo": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/514/778/medium/BRETTONE_LOGO2.jpg"
+        },
+        "name": "Brettone Catering",
+        "services": [
+            {
+                "defaultChargeAmount": 4.95,
+                "defaultChargePercent": 0,
+                "defaultTime": 30,
+                "description": "Delivery orders for this restaurant are delivered directly by DoorDash. (855) 973-1040 You may receive an sms to track your delivery order.\n",
+                "id": "DELIVERY",
+                "min_order": 20
+            },
+            {
+                "defaultChargeAmount": null,
+                "defaultChargePercent": null,
+                "defaultTime": null,
+                "description": "",
+                "id": "PICKUP",
+                "min_order": 0
+            }
+        ],
+        "settings": {
+            "averagePrice": null,
+            "pause_online_ordering": true,
+            "preparationTime": 30
+        },
+        "timezone": {
+            "offset": -300
+        }
+    },
+    {
+        "cuisines": "Vegan, Juice Bar, Smoothies, Juices, Vegetarian, Healthy",
+        "dietary_preferences": [
+            "Gluten Free",
+            "Diabetic",
+            "Low Calorie",
+            "Vegan",
+            "Vegetarian",
+            "Organic"
+        ],
+        "featuredImage": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/502/731/medium/freshouse.png"
+        },
+        "hoursOfOperation": [
+            [
+                [
+                    540,
+                    1110
+                ]
+            ],
+            [
+                [
+                    480,
+                    1170
+                ]
+            ],
+            [
+                [
+                    480,
+                    1170
+                ]
+            ],
+            [
+                [
+                    480,
+                    1170
+                ]
+            ],
+            [
+                [
+                    480,
+                    1170
+                ]
+            ],
+            [
+                [
+                    480,
+                    1170
+                ]
+            ],
+            [
+                [
+                    540,
+                    1110
+                ]
+            ]
+        ],
+        "id": "13565",
+        "integrations": [
+            {
+                "id": "80331",
+                "location": null,
+                "remote_id": "freshousefoodandjuicebar"
+            }
+        ],
+        "locations": [
+            {
+                "address": {
+                    "geo": {
+                        "lat": 43.782288,
+                        "lng": -79.573733
+                    },
+                    "nickname": "2 Tall Grass Trail Unit 1, Woodbridge, ON L4L 3Y9, Canada"
+                },
+                "id": "14966"
+            }
+        ],
+        "logo": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/485/711/medium/Freshouse_Food_and_Juice_Bar.jpg"
+        },
+        "name": "Freshouse Food and Juice Bar",
+        "services": [
+            {
+                "defaultChargeAmount": 4.95,
+                "defaultChargePercent": 0,
+                "defaultTime": 20,
+                "description": "Delivery orders for this restaurant are delivered directly by DoorDash. (855) 973-1040 You may receive an sms to track your delivery order.\n",
+                "id": "DELIVERY",
+                "min_order": 20
+            },
+            {
+                "defaultChargeAmount": null,
+                "defaultChargePercent": null,
+                "defaultTime": null,
+                "description": "",
+                "id": "PICKUP",
+                "min_order": 0
+            }
+        ],
+        "settings": {
+            "averagePrice": null,
+            "pause_online_ordering": false,
+            "preparationTime": 10
+        },
+        "timezone": {
+            "offset": -300
+        }
+    },
+    {
+        "cuisines": "Italian, Pizza, Mediterranean, Vegan, Salads, Pasta, Chicken, Stone Baked Pizza, Antipasta, Lasagna, Gluten free",
+        "dietary_preferences": [
+            "Vegetarian",
+            "Vegan",
+            "Gluten Free"
+        ],
+        "featuredImage": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/583/996/medium/full_spread_of_pasta___pizza___chicken_parm___costa___photo_credit_anthony_mair_low.jpg"
+        },
+        "hoursOfOperation": [
+            [],
+            [],
+            [
+                [
+                    690,
+                    1260
+                ]
+            ],
+            [
+                [
+                    690,
+                    1260
+                ]
+            ],
+            [
+                [
+                    690,
+                    1260
+                ]
+            ],
+            [
+                [
+                    690,
+                    1290
+                ]
+            ],
+            [
+                [
+                    960,
+                    1290
+                ]
+            ]
+        ],
+        "id": "14824",
+        "integrations": [
+            {
+                "id": "85062",
+                "location": null,
+                "remote_id": "labellaitalia"
+            }
+        ],
+        "locations": [
+            {
+                "address": {
+                    "geo": {
+                        "lat": 43.837929,
+                        "lng": -79.509272
+                    },
+                    "nickname": "9300 Keele St, Maple, ON L6A 1P4, Canada"
+                },
+                "id": "16465"
+            }
+        ],
+        "logo": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/588/431/medium/LaBella1.4.png"
+        },
+        "name": "La Bella Italia",
+        "services": [
+            {
+                "defaultChargeAmount": 4.95,
+                "defaultChargePercent": 0,
+                "defaultTime": 20,
+                "description": "Delivery orders for this restaurant are delivered directly by DoorDash. (855) 973-1040 You may receive an sms to track your delivery order.",
+                "id": "DELIVERY",
+                "min_order": 25
+            },
+            {
+                "defaultChargeAmount": null,
+                "defaultChargePercent": null,
+                "defaultTime": null,
+                "description": "",
+                "id": "PICKUP",
+                "min_order": 0
+            },
+            {
+                "defaultChargeAmount": null,
+                "defaultChargePercent": null,
+                "defaultTime": null,
+                "description": "",
+                "id": "CURBSIDE",
+                "min_order": null
+            }
+        ],
+        "settings": {
+            "averagePrice": 0,
+            "pause_online_ordering": false,
+            "preparationTime": 25
+        },
+        "timezone": {
+            "offset": -300
+        }
+    },
+    {
+        "cuisines": "Indian, Vegan, Curry, Chicken, Soups, Lunch",
+        "dietary_preferences": [
+            "Vegetarian",
+            "Vegan"
+        ],
+        "featuredImage": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/598/077/medium/punjaabi_food.jpg"
+        },
+        "hoursOfOperation": [
+            [
+                [
+                    690,
+                    1305
+                ]
+            ],
+            [],
+            [
+                [
+                    690,
+                    1305
+                ]
+            ],
+            [
+                [
+                    690,
+                    1305
+                ]
+            ],
+            [
+                [
+                    690,
+                    1305
+                ]
+            ],
+            [
+                [
+                    690,
+                    1305
+                ]
+            ],
+            [
+                [
+                    690,
+                    1305
+                ]
+            ]
+        ],
+        "id": "15000",
+        "integrations": [
+            {
+                "id": "85067",
+                "location": null,
+                "remote_id": "punjaabisweetsrestaurant"
+            }
+        ],
+        "locations": [
+            {
+                "address": {
+                    "geo": {
+                        "lat": 43.790763,
+                        "lng": -79.552305
+                    },
+                    "nickname": "150 Windflower Gate, Woodbridge, ON L4L 9K8, Canada"
+                },
+                "id": "16440"
+            }
+        ],
+        "logo": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/564/472/medium/Untitled_(6).jpg"
+        },
+        "name": "Punjaabi Sweets & Restaurant",
+        "services": [
+            {
+                "defaultChargeAmount": 4.95,
+                "defaultChargePercent": 0,
+                "defaultTime": 20,
+                "description": "Delivery orders for this restaurant are delivered directly by DoorDash. (855) 973-1040 You may receive an sms to track your delivery order.\n",
+                "id": "DELIVERY",
+                "min_order": 25
+            },
+            {
+                "defaultChargeAmount": null,
+                "defaultChargePercent": null,
+                "defaultTime": null,
+                "description": "",
+                "id": "PICKUP",
+                "min_order": 0
+            },
+            {
+                "defaultChargeAmount": null,
+                "defaultChargePercent": null,
+                "defaultTime": null,
+                "description": "",
+                "id": "CURBSIDE",
+                "min_order": null
+            }
+        ],
+        "settings": {
+            "averagePrice": null,
+            "pause_online_ordering": false,
+            "preparationTime": 20
+        },
+        "timezone": {
+            "offset": -300
+        }
+    },
+    {
+        "cuisines": "Italian, Pizza, Mediterranean, Vegan, Salads, Sandwiches, Pasta, Stone Baked Pizza, Wraps, Vegetarian",
+        "dietary_preferences": [],
+        "featuredImage": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/529/146/medium/catering_meals-crop.jpg"
+        },
+        "hoursOfOperation": [
+            [
+                [
+                    405,
+                    1320
+                ]
+            ],
+            [
+                [
+                    405,
+                    900
+                ],
+                [
+                    960,
+                    1320
+                ]
+            ],
+            [
+                [
+                    405,
+                    900
+                ],
+                [
+                    960,
+                    1320
+                ]
+            ],
+            [
+                [
+                    405,
+                    900
+                ],
+                [
+                    960,
+                    1320
+                ]
+            ],
+            [
+                [
+                    405,
+                    900
+                ],
+                [
+                    960,
+                    1320
+                ]
+            ],
+            [
+                [
+                    405,
+                    900
+                ],
+                [
+                    960,
+                    1320
+                ]
+            ],
+            [
+                [
+                    405,
+                    900
+                ],
+                [
+                    960,
+                    1320
+                ]
+            ]
+        ],
+        "id": "14545",
+        "integrations": [
+            {
+                "id": "83529",
+                "location": null,
+                "remote_id": "thatsitalianristorantecatering"
+            }
+        ],
+        "locations": [
+            {
+                "address": {
+                    "geo": {
+                        "lat": 43.782288,
+                        "lng": -79.573733
+                    },
+                    "nickname": "2 Tall Grass Trail, Woodbridge, ON L4L 3Y9, Canada"
+                },
+                "id": "15954"
+            }
+        ],
+        "logo": {
+            "medium": "https://dbgcbnch6yz43.cloudfront.net/images/pictures/files/000/529/145/medium/thats_italian_catering_logo.png"
+        },
+        "name": "That's Italian Catering - Foodiee",
+        "services": [
+            {
+                "defaultChargeAmount": 10,
+                "defaultChargePercent": 0,
+                "defaultTime": 2160,
+                "description": "We deliver within the Greater Toronto Area. Delivery fees vary depending on distance. We will contact you with the final amount and with a payment link.\n",
+                "id": "DELIVERY",
+                "min_order": 100
+            },
+            {
+                "defaultChargeAmount": null,
+                "defaultChargePercent": null,
+                "defaultTime": null,
+                "description": "",
+                "id": "PICKUP",
+                "min_order": 0
+            }
+        ],
+        "settings": {
+            "averagePrice": null,
+            "pause_online_ordering": false,
+            "preparationTime": 0
+        },
+        "timezone": {
+            "offset": -300
+        }
+    }
+]
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className='logo' src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = (props) => {
+    console.log(props.data)
+    return (
+        <div className='res-card'>
+            <img className='res-card-img' src={props.data.logo.medium} />
+
+            <h3>{props.data.name}</h3>
+            <p>{props.data.cuisines}</p>
+            <p>4.3 stars</p>
+            <p>38 mins</p>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (<>
+        <div className='body'>
+            <div className='search'>
+                Search
+            </div>
+            <div className='res-container'>
+                {restaurantList.map((restaurant, index) => (<RestaurantCard data={restaurant} />))}
+            </div>
+        </div>
+    </>)
+}
+
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<AppLayout />)
---
+++ README.md
@@ -0,0 +1,16 @@
+# React Web Application
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused folder
